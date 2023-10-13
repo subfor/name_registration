069e49bd2cca0dd5ad997eb646e2600fd0013266
alex <alex.kudelko@gmail.com>
2023-10-13 16:46:39 +0300

    remove Lock from deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,38 +7,10 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const DomainRegistration = await hre.ethers.deployContract("DomainRegistration");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-
-const hre = require("hardhat");
-
-async function main() {
-
-  const counter = await hre.ethers.deployContract("DomainRegistration");
-
-  await counter.waitForDeployment();
+  await DomainRegistration.waitForDeployment();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
